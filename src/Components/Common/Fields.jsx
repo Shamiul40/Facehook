654217326f63603ca6c61ce6e4e2780223bdda08
Shamiul40 <shamiul4040@gmail.com>
2026-01-21 01:08:24 +0600

    react hook form implement

--- src/Components/Common/Fields.jsx
+++ src/Components/Common/Fields.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function Fields({children, label, error, htmlFor}) {
-  return (
+    
+    return (
     <div>
-      {label && <label>{label}</label>}
+      {label && <label className='text-white py-2' htmlFor='email'>{label}</label>}
       {children}
       {error && <div className='text-red-400'>{error.message}</div>}
     </div>
--- src/auth/LoginForm.jsx
+++ src/auth/LoginForm.jsx
@@ -1,27 +1,54 @@
-import React from 'react'
+import React from "react";
+import Fields from "../Components/Common/Fields";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
 
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  
+  const navigate = useNavigate()
+
+  const onSubmitForm=(formData)=>{
+    navigate("/")
+    console.log(formData)
+  }
+
   return (
-    <form className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]">
+    <form onSubmit={handleSubmit(onSubmitForm)} className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]">
       {/* <!-- email --> */}
       <div className="mb-6">
-        <label className="block text-sm lg:text-lg leading-[138%] text-white mb-2" htmlFor="email">Email</label>
-        <input
-          className="w-full rounded-md border border-gray-400/10  p-3 lg:p-4 text-white focus:outline-none focus:ring-2 "
-          name="email"
-          type="email"
-          id="email"
-        />
+        <Fields label={"email"} htmlFor={"email"} error={errors}>
+          <input
+           {...register("email", {required : "email is required"})}
+
+            className="w-full rounded-md border border-gray-400/10  p-3 lg:p-4 text-white focus:outline-none focus:ring-2 "
+            name="email"
+            type="email"
+            id="email"
+          />
+        </Fields>
       </div>
       {/* <!-- password --> */}
       <div className="mb-6">
-        <label className="block text-sm lg:text-lg leading-[138%] text-white mb-2" htmlFor="password">Password</label>
-        <input
-          className="w-full rounded-md border border-gray-400/10 bg-lighterDark p-3 lg:p-4 text-white focus:outline-none focus:ring-2 focus:ring-lwsGreen"
-          name="password"
-          type="password"
-          id="password"
-        />
+        <Fields label={"password"} htmlFor={"password"} error={errors}>
+          <input
+           {...register("password", {required : "password is required",
+            minLength :{
+                value : 8,
+                message : "password must be atleast 8 character"
+            }
+           })}
+
+            className="w-full rounded-md border border-gray-400/10  p-3 lg:p-4 text-white focus:outline-none focus:ring-2 "
+            name="password"
+            type="password"
+            id="password"
+          />
+        </Fields>
       </div>
       {/* <!-- Submit --> */}
       <button
@@ -31,5 +58,5 @@ export default function LoginForm() {
         Login
       </button>
     </form>
-  )
+  );
 }
