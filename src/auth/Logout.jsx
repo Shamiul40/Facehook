f0cebd2dbac90107c3fe322014a16e37d7d069bd
Shamiul40 <shamiul4040@gmail.com>
2026-01-21 20:41:56 +0600

    logout

--- src/auth/Logout.jsx
+++ src/auth/Logout.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import logoutImg from "../assets/icons/logout.svg"
+import { useNavigate } from 'react-router'
 
 export default function Logout() {
+    const navigate = useNavigate()
+
+    const handleLogout=()=>{
+        navigate("/login")
+    }
+
   return (
     <button>
-      <img src={logoutImg} alt="logout" />
+      <img onClick={handleLogout} src={logoutImg} alt="logout" />
     </button>
   )
 }
